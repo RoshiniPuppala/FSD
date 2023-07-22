a519853f6a552990cc7b05dcc291fcffe4dc8f92
Roshni <124174302+RoshiniPuppala@users.noreply.github.com>
2023-07-22 23:03:00 +0530

    Updated queries

--- MovieDetails.sql
+++ MovieDetails.sql
@@ -1,18 +1,18 @@
-/*creating and insserting data into  Movies table*/
+/*creating and inserting data into  Movies table*/
 CREATE TABLE Movies(movieId int(10) PRIMARY KEY, title varchar(20), release_year varchar(20), director_Id int(10));
-INSERT INTO Movies VALUES (101, "Bahubali1", '2015', 1101),(102, "Mirchi", '2013', 1102),(103, "Rebel", '2012', 1103),(104, "Billa", '2009', 1104);
+INSERT INTO Movies VALUES (101, "Bahubali", '2015', 1101),(102, "Mirchi", '2013', 1102),(103, "Rebel", '2012', 1103),(104, "Billa", '2009', 1104);
 
-/*creating and insserting data into Directors table*/
+/*creating and inserting data into Directors table*/
 CREATE TABLE Directors (director_Id int(10) PRIMARY KEY, director_name varchar(20));
 INSERT INTO Directors VALUES (1101, "Rajamouli"),(1102, "Koratala Shiva"),(1103, "Raghava Lawrence"),(1105, "Mehar Ramesh");
 
-/*creating and insserting data into Genres table*/
+/*creating and inserting data into Genres table*/
 CREATE TABLE Genres (genre_Id int(10) PRIMARY KEY, genre_name varchar(20));
 INSERT INTO Genres VALUES (191,"Drama"),(192, "Rom-Com"),(193, "Action"),(194, "Triller");
 
-/*creating and insserting data into Movie_Genres*/
+/*creating and inserting data into Movie_Genres*/
 CREATE TABLE Movie_Genres (movie_Id int(10) PRIMARY KEY, genre_Id int(10));
-INSERT INTO mMovie_Genres VALUES (101, 191),(102, 192);,(103, 1993);
+INSERT INTO Movie_Genres VALUES (101, 191),(102, 192),(103, 193);
 
 
 /*Write a query to retrieve the movie title and the corresponding director name for all movies.*/
